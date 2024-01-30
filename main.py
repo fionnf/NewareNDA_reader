7dd303f0a9884447993cedf656ff9703f9762469
F.M. Eckardt Ferreira <f.m.eckardt.ferreira@student.rug.nl>
2024-01-30 14:01:25 +0100

    Read an NDA file and print it as a CSV file to same directory

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import NewareNDA as nda
+import pandas as pd
+import os
 
 def print_ndax_as_csv(file_path):
     data = nda.read(file_path)
-    print(data.to_csv())
+    df = pd.DataFrame(data)
+    base_name = os.path.splitext(os.path.basename(file_path))[0]
+    output_csv_path = os.path.join(os.path.dirname(file_path), base_name + '.csv')
+    df.to_csv(output_csv_path, index=False)
+
+    print(f'Data printed to CSV file: {output_csv_path}')
 
 print_ndax_as_csv(r"G:\.shortcut-targets-by-id\1gpf-XKVVvMHbMGqpyQS5Amwp9fh8r96B\RUG shared\Master Project\Experiment files\FF041\FF041Batt_b.ndax")
